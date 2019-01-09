6156dbf76ecce4a6e870b1c2dbf9ec32996556c7
ph0814 <46508920+ph0814@users.noreply.github.com>
2019-01-09 10:23:56 +0700

    Update ytads.sh

--- ytads.sh
+++ ytads.sh
@@ -20,17 +20,17 @@ wait
 
 # Replace r*.sn*.googlevideo.com URLs to r*---sn-*.googlevideo.com
 # and add those to the list too
-sudo cat $BLACKLIST | sed -r 's/(^r[[:digit:]]+)(\.)(sn)/\1---\3-/' >> $BLACKLIST
+sudo cat $BLACKLIST | sed -r 's/(^r[[:digit:]]+)(\.)(sn)/\1---\3/' >> $BLACKLIST
 
-sudo cat $BLACKLISTTXT | sed -r 's/(^r[[:digit:]]+)(\.)(sn)/\1---\3-/' >> $BLACKLISTTXT
+sudo cat $BLACKLISTTXT | sed -r 's/(^r[[:digit:]]+)(\.)(sn)/\1---\3/' >> $BLACKLISTTXT
 
 # Scan log file for previously accessed domains
-grep '^r[0-9]*.*googlevideo\.com' /var/log/pihole*.log \
+grep '^r([1-9]|1\d|20).*-.*\.googlevideo\.com' /var/log/pihole*.log \
 | awk '{print $8}' \
 | grep -vE "redirector|manifest" \
 | sort | uniq >> $BLACKLIST
 
-grep '^r[0-9]*.*googlevideo\.com' /var/log/pihole*.log \
+grep '^r([1-9]|1\d|20).*-.*\.googlevideo\.com' /var/log/pihole*.log \
 | awk '{print $8}' \
 | grep -vE "redirector|manifest" \
 | sort | uniq >> $BLACKLISTTXT
