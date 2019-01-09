799893a85d22c041e9b2a8bfbbb190eec4e7c78a
ph0814 <46508920+ph0814@users.noreply.github.com>
2019-01-09 09:43:09 +0700

    Create ytads.sh

---
+++ ytads.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+# This script will fetch the Googlevideo ad domains and append them to the Pi-hole block list.
+# Run this script daily with a cron job (don't forget to chmod +x)
+# 0 * / 4 * * * sudo /home/piscript/ytads.sh> / dev / null
+
+# File to store the YT ad domains
+BLACKLIST='/etc/pihole/black.list'
+BLACKLISTTXT='/etc/pihole/blacklist.txt'
+
+# Fetch the list of domains, remove the ip's and save them
+sudo curl --silent 'https://api.hackertarget.com/hostsearch/?q=googlevideo.com' \
+| awk -F, 'NR>1{print $1}' \
+| grep -vE "redirector|manifest" > $BLACKLIST
+
+sudo curl --silent 'https://api.hackertarget.com/hostsearch/?q=googlevideo.com' \
+| awk -F, 'NR>1{print $1}' \
+| grep -vE "redirector|manifest" > $BLACKLISTTXT
+
+wait
+
+# Replace r*.sn*.googlevideo.com URLs to r*---sn-*.googlevideo.com
+# and add those to the list too
+sudo cat $BLACKLIST | sed -r 's/(^r[[:digit:]]+)(\.)(sn)/\1---\3-/' >> $BLACKLIST
+
+sudo cat $BLACKLISTTXT | sed -r 's/(^r[[:digit:]]+)(\.)(sn)/\1---\3-/' >> $BLACKLISTTXT
+
+# Scan log file for previously accessed domains
+grep '^r.*googlevideo\.com' /var/log/pihole*.log \
+| awk '{print $8}' \
+| grep -vE "redirector|manifest" \
+| sort | uniq >> $BLACKLIST
+
+grep '^r.*googlevideo\.com' /var/log/pihole*.log \
+| awk '{print $8}' \
+| grep -vE "redirector|manifest" \
+| sort | uniq >> $BLACKLISTTXT
+
+#wait
+
+# check to see if gawk is installed. if not it will install it
+#dpkg -l | grep -qw gawk || sudo apt-get install gawk
+
+wait
+
+# remove the duplicate records in place
+gawk -i inplace '!a[$0]++' $BLACKLIST
+
+gawk -i inplace '!a[$0]++' $BLACKLISTTXT
